4c6d7e22395f83cba3e898abe9e4f1c3a5810481
ianmartinez <ianmartinez97@live.com>
2017-07-10 17:25:25 -0500

    Small fixes

---
+++ Thorn/filedata.pas
@@ -0,0 +1,20 @@
+unit FileData;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+ThFile = record
+   title: string;
+   author: string;
+   website: string;
+end;
+
+implementation
+
+end.
+
---
+++ Thorn/thornfile.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ThornFile;
+
+interface
+
+uses
+  FileData, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ThornFile', @Register);
+end.
