9002f202ecfc8d90c31634bb56347cd513db7fd5
ianmartinez <ianmartinez97@live.com>
2017-07-09 21:01:11 -0500

    Initial

---
+++ Thorn/unit1.pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MainMenu: TMainMenu;
+    FileMenu: TMenuItem;
+    FileSep2: TMenuItem;
+    SaveMenuItem: TMenuItem;
+    SaveAsMenuItem: TMenuItem;
+    NewMenuItem: TMenuItem;
+    FileSep1: TMenuItem;
+    FileSep3: TMenuItem;
+    ExitMenuItem: TMenuItem;
+    OpenMenuItem: TMenuItem;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+end.
+
