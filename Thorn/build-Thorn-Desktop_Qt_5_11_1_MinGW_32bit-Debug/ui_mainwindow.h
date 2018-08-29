ec25e8a03a03a22707d41114ea23ab41772d3703
Ian Martinez <ianmartinez97@live.com>
2018-08-29 11:47:41 -0500

    Basic layout

--- Thorn/build-Thorn-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_mainwindow.h
+++ Thorn/build-Thorn-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,11 +13,13 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QDockWidget>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QTreeWidget>
 #include <QtWidgets/QVBoxLayout>
@@ -29,11 +31,16 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
+    QHBoxLayout *horizontalLayout;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
     QMenuBar *menuBar;
     QMenu *menu_File;
     QMenu *menu_Edit;
     QMenu *menuNotebook;
     QMenu *menuDictionary;
+    QMenu *menu_Help;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
     QDockWidget *dockWidget;
@@ -45,13 +52,28 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(955, 635);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayout = new QHBoxLayout(centralWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tab = new QWidget();
+        tab->setObjectName(QStringLiteral("tab"));
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+
+        horizontalLayout->addWidget(tabWidget);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 21));
+        menuBar->setGeometry(QRect(0, 0, 955, 21));
         menu_File = new QMenu(menuBar);
         menu_File->setObjectName(QStringLiteral("menu_File"));
         menu_Edit = new QMenu(menuBar);
@@ -60,6 +82,8 @@ public:
         menuNotebook->setObjectName(QStringLiteral("menuNotebook"));
         menuDictionary = new QMenu(menuBar);
         menuDictionary->setObjectName(QStringLiteral("menuDictionary"));
+        menu_Help = new QMenu(menuBar);
+        menu_Help->setObjectName(QStringLiteral("menu_Help"));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -69,6 +93,7 @@ public:
         MainWindow->setStatusBar(statusBar);
         dockWidget = new QDockWidget(MainWindow);
         dockWidget->setObjectName(QStringLiteral("dockWidget"));
+        dockWidget->setMinimumSize(QSize(300, 105));
         dockWidgetContents = new QWidget();
         dockWidgetContents->setObjectName(QStringLiteral("dockWidgetContents"));
         verticalLayout = new QVBoxLayout(dockWidgetContents);
@@ -77,9 +102,17 @@ public:
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
         treeWidget = new QTreeWidget(dockWidgetContents);
         QIcon icon;
-        icon.addFile(QStringLiteral("../../icons/breeze/Icons/application-msonenote.svg"), QSize(), QIcon::Normal, QIcon::Off);
+        icon.addFile(QStringLiteral("../../icons/breeze/Icons/edit-group.svg"), QSize(), QIcon::Normal, QIcon::Off);
+        QIcon icon1;
+        icon1.addFile(QStringLiteral("../../icons/breeze/Icons/view-calendar-agenda.svg"), QSize(), QIcon::Normal, QIcon::Off);
+        QIcon icon2;
+        icon2.addFile(QStringLiteral("../../icons/breeze/Icons/folder.svg"), QSize(), QIcon::Normal, QIcon::Off);
         QTreeWidgetItem *__qtreewidgetitem = new QTreeWidgetItem(treeWidget);
         __qtreewidgetitem->setIcon(0, icon);
+        QTreeWidgetItem *__qtreewidgetitem1 = new QTreeWidgetItem(treeWidget);
+        __qtreewidgetitem1->setIcon(0, icon1);
+        QTreeWidgetItem *__qtreewidgetitem2 = new QTreeWidgetItem(treeWidget);
+        __qtreewidgetitem2->setIcon(0, icon2);
         treeWidget->setObjectName(QStringLiteral("treeWidget"));
         QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         sizePolicy.setHorizontalStretch(0);
@@ -98,6 +131,7 @@ public:
         menuBar->addAction(menu_Edit->menuAction());
         menuBar->addAction(menuNotebook->menuAction());
         menuBar->addAction(menuDictionary->menuAction());
+        menuBar->addAction(menu_Help->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -107,17 +141,24 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "Tab 1", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "Tab 2", nullptr));
         menu_File->setTitle(QApplication::translate("MainWindow", "&File", nullptr));
         menu_Edit->setTitle(QApplication::translate("MainWindow", "&Edit", nullptr));
         menuNotebook->setTitle(QApplication::translate("MainWindow", "&Notebook", nullptr));
         menuDictionary->setTitle(QApplication::translate("MainWindow", "&Dictionary", nullptr));
+        menu_Help->setTitle(QApplication::translate("MainWindow", "&Help", nullptr));
         QTreeWidgetItem *___qtreewidgetitem = treeWidget->headerItem();
-        ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "1", nullptr));
+        ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "Project", nullptr));
 
         const bool __sortingEnabled = treeWidget->isSortingEnabled();
         treeWidget->setSortingEnabled(false);
         QTreeWidgetItem *___qtreewidgetitem1 = treeWidget->topLevelItem(0);
         ___qtreewidgetitem1->setText(0, QApplication::translate("MainWindow", "Pages", nullptr));
+        QTreeWidgetItem *___qtreewidgetitem2 = treeWidget->topLevelItem(1);
+        ___qtreewidgetitem2->setText(0, QApplication::translate("MainWindow", "Dictionary", nullptr));
+        QTreeWidgetItem *___qtreewidgetitem3 = treeWidget->topLevelItem(2);
+        ___qtreewidgetitem3->setText(0, QApplication::translate("MainWindow", "Resources", nullptr));
         treeWidget->setSortingEnabled(__sortingEnabled);
 
     } // retranslateUi
