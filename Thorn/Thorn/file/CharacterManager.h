1e5f0c3d3cc5a7b8b97eedaa52808860e2546915
Ian Martinez <ianmartinez97@live.com>
2018-10-05 20:48:08 -0500

    Add elder futhark

--- Thorn/Thorn/file/CharacterManager.h
+++ Thorn/Thorn/file/CharacterManager.h
@@ -41,21 +41,23 @@ public:
      *      languages that do not have keyboards
      *      in most operating systems,
      *      but have decent font representation
-     *      I.E. Germanic runes or Carian */
-    vector<Character> ScriptCarian; // https://en.wikipedia.org/wiki/Carian_(Unicode_block)
+     *      I.E. Germanic runes.
+     *      Scripts commented out sadly don't have decent
+     *      enough font support cross platform. */
+    // vector<Character> ScriptCarian; // https://en.wikipedia.org/wiki/Carian_(Unicode_block)
     vector<Character> ScriptCoptic; // https://en.wikipedia.org/wiki/Coptic_alphabet
-    vector<Character> ScriptOldItalic; // https://en.wikipedia.org/wiki/Old_Italic_script#Etruscan_alphabet
-    vector<Character> ScriptGlagolitic; // https://en.wikipedia.org/wiki/Glagolitic_script
-    vector<Character> ScriptGothic; // https://en.wikipedia.org/wiki/Gothic_alphabet
-    vector<Character> ScriptLydian; // https://en.wikipedia.org/wiki/Lydian_alphabet
-    vector<Character> ScriptLycian; // https://en.wikipedia.org/wiki/Lycian_alphabet
-    vector<Character> ScriptOgham; // https://en.wikipedia.org/wiki/Ogham
-    vector<Character> ScriptOldTurkic; // https://en.wikipedia.org/wiki/Old_Turkic_alphabet
-    vector<Character> ScriptOsage; // https://en.wikipedia.org/wiki/Osage_alphabet
-    vector<Character> ScriptOsmanya; // https://en.wikipedia.org/wiki/Osmanya_alphabet
-    vector<Character> ScriptRunic; // https://en.wikipedia.org/wiki/Runes
-    vector<Character> ScriptPhoenician; // https://en.wikipedia.org/wiki/Phoenician_alphabet
-    vector<Character> ScriptSouthArabian; // https://en.wikipedia.org/wiki/Ancient_South_Arabian_script
+    // vector<Character> ScriptOldItalic; // https://en.wikipedia.org/wiki/Old_Italic_script#Etruscan_alphabet
+    // vector<Character> ScriptGlagolitic; // https://en.wikipedia.org/wiki/Glagolitic_script
+    // vector<Character> ScriptGothic; // https://en.wikipedia.org/wiki/Gothic_alphabet
+    // vector<Character> ScriptLydian; // https://en.wikipedia.org/wiki/Lydian_alphabet
+    // vector<Character> ScriptLycian; // https://en.wikipedia.org/wiki/Lycian_alphabet
+    // vector<Character> ScriptOgham; // https://en.wikipedia.org/wiki/Ogham
+    // vector<Character> ScriptOldTurkic; // https://en.wikipedia.org/wiki/Old_Turkic_alphabet
+    // vector<Character> ScriptOsage; // https://en.wikipedia.org/wiki/Osage_alphabet
+    // vector<Character> ScriptOsmanya; // https://en.wikipedia.org/wiki/Osmanya_alphabet
+    vector<Character> ScriptElderFuthark; // https://en.wikipedia.org/wiki/Elder_Futhark
+    // vector<Character> ScriptPhoenician; // https://en.wikipedia.org/wiki/Phoenician_alphabet
+    // vector<Character> ScriptSouthArabian; // https://en.wikipedia.org/wiki/Ancient_South_Arabian_script
 
 
     vector<Character> GetCharactersFromResource(QString ResourceName);
